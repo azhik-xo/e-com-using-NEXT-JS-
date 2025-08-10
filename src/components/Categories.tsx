4f7cd0cf9f90d3c22f0d179a610e580f8662acc9
Abdul Ashik <syncthrive.meh@gmail.com>
2025-08-10 12:44:37 +0530

    first commit

---
+++ src/components/Categories.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {
+  Footprints,
+  Glasses,
+  Briefcase,
+  Shirt,
+  ShoppingBasket,
+  Hand,
+  Venus,
+} from "lucide-react";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const categories = [
+  {
+    name: "All",
+    icon: <ShoppingBasket className="w-4 h-4" />,
+    slug: "all",
+  },
+  {
+    name: "T-shirts",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "t-shirts",
+  },
+  {
+    name: "Shoes",
+    icon: <Footprints className="w-4 h-4" />,
+    slug: "shoes",
+  },
+  {
+    name: "Accessories",
+    icon: <Glasses className="w-4 h-4" />,
+    slug: "accessories",
+  },
+  {
+    name: "Bags",
+    icon: <Briefcase className="w-4 h-4" />,
+    slug: "bags",
+  },
+  {
+    name: "Dresses",
+    icon: <Venus className="w-4 h-4" />,
+    slug: "dresses",
+  },
+  {
+    name: "Jackets",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "jackets",
+  },
+  {
+    name: "Gloves",
+    icon: <Hand className="w-4 h-4" />,
+    slug: "gloves",
+  },
+];
+
+const Categories = () => {
+  return (
+    <div className="grid gird-cols-2 sm:gird-cols-3 md:gird-cols-4 lg:gird-cols-6 xl:gird-cols-8 gap-2 bg-gray-100 p-2 rounded-lg ">
+      Category
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/components/ProductCards.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { ProductType } from "@/types";
+
+const ProductCards = ({ product }: { product: ProductType }) => {
+  return <div>Product</div>;
+};
+
+export default ProductCards;
