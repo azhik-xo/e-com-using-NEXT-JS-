0777add7457adecabf6dba4e58dde9c34b01d502
Abdul Ashik <syncthrive.meh@gmail.com>
2025-08-10 13:45:18 +0530

    second Commit (Categories completed)

--- src/components/ProductCards.tsx
+++ src/components/ProductCards.tsx
@@ -1,9 +1,30 @@
 "use client";
 
 import { ProductType } from "@/types";
+import Link from "next/link";
+import Image from "next/image";
 
 const ProductCards = ({ product }: { product: ProductType }) => {
-  return <div>Product</div>;
+  return (
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      {/* IMAGE */}
+      <Link href={`/product/${product.id}`}>
+        <div className="relative aspect-[2/3]">
+          <Image
+            src={product.images[product.colors[0]]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-all duration-300"
+          />
+        </div>
+      </Link>
+
+      {/* PRODUCT DETAIL */}
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font"></h1>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCards;
